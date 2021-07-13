ed75320b40ca8167002bd17c0bb8bd7ebed37dfc
Peter Keating <pete.keating@etchuk.com>
2021-07-13 22:28:17 +0100

    Upgrade to 1.0.0

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -24,6 +24,7 @@ namespace Etch.OrchardCore.TinyPNG.Controllers
         private readonly IContentTypeProvider _contentTypeProvider;
         private readonly ILogger _logger;
         private readonly IMediaFileStore _mediaFileStore;
+        private readonly IMediaNameNormalizerService _mediaNameNormalizerService;
         private readonly IStringLocalizer S;
         private readonly ITinyPngService _tinyPngService;
 
@@ -32,6 +33,7 @@ namespace Etch.OrchardCore.TinyPNG.Controllers
             IContentTypeProvider contentTypeProvider,
             ILogger<AdminController> logger,
             IMediaFileStore mediaFileStore,
+            IMediaNameNormalizerService mediaNameNormalizerService,
             IOptions<MediaOptions> options,
             IStringLocalizer<AdminController> stringLocalizer,
             ITinyPngService tinyPngService
@@ -42,6 +44,7 @@ namespace Etch.OrchardCore.TinyPNG.Controllers
             _contentTypeProvider = contentTypeProvider;
             _logger = logger;
             _mediaFileStore = mediaFileStore;
+            _mediaNameNormalizerService = mediaNameNormalizerService;
             S = stringLocalizer;
             _tinyPngService = tinyPngService;
         }
@@ -53,7 +56,7 @@ namespace Etch.OrchardCore.TinyPNG.Controllers
             string path,
             ICollection<IFormFile> files)
         {
-            if (!await _authorizationService.AuthorizeAsync(User, Permissions.ManageOwnMedia))
+            if (!await _authorizationService.AuthorizeAsync(User, Permissions.ManageMedia))
             {
                 return Forbid();
             }
@@ -68,6 +71,8 @@ namespace Etch.OrchardCore.TinyPNG.Controllers
             // Loop through each file in the request
             foreach (var file in files)
             {
+                // TODO: support clipboard
+
                 if (!_allowedFileExtensions.Contains(Path.GetExtension(file.FileName), StringComparer.OrdinalIgnoreCase))
                 {
                     result.Add(new
@@ -78,30 +83,31 @@ namespace Etch.OrchardCore.TinyPNG.Controllers
                         error = S["This file extension is not allowed: {0}", Path.GetExtension(file.FileName)].ToString()
                     });
 
-                    _logger.LogInformation("File extension not allowed: '{0}'", file.FileName);
+                    _logger.LogInformation("File extension not allowed: '{File}'", file.FileName);
 
                     continue;
                 }
 
+                var fileName = _mediaNameNormalizerService.NormalizeFileName(file.FileName);
+
                 Stream stream = null;
                 try
                 {
-                    var mediaFilePath = _mediaFileStore.Combine(path, file.FileName);
+                    var mediaFilePath = _mediaFileStore.Combine(path, fileName);
                     stream = file.OpenReadStream();
-
                     mediaFilePath = await _mediaFileStore.CreateFileFromStreamAsync(mediaFilePath, await _tinyPngService.OptimiseAsync(stream, file.FileName));
 
                     var mediaFile = await _mediaFileStore.GetFileInfoAsync(mediaFilePath);
 
                     result.Add(CreateFileResult(mediaFile));
                 }
-                catch (System.Exception ex)
+                catch (Exception ex)
                 {
                     _logger.LogError(ex, "An error occurred while uploading a media");
 
                     result.Add(new
                     {
-                        name = file.FileName,
+                        name = fileName,
                         size = file.Length,
                         folder = path,
                         error = ex.Message
