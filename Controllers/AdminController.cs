9ff03686d69f97f04bd2f44684469c882a8316a4
Peter Keating <pete.keating@etchuk.com>
2023-11-28 17:25:42 +0000

    Upgrade Orchard Core to 1.7.2

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,4 @@
-﻿using Etch.OrchardCore.TinyPNG.Services;
+using Etch.OrchardCore.TinyPNG.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,17 +19,22 @@ namespace Etch.OrchardCore.TinyPNG.Controllers
 {
     public class AdminController : Controller
     {
-        private readonly HashSet<string> _allowedFileExtensions;
+        private static readonly char[] ExtensionSeperator = new char[] { ' ', ',' };
+        private static readonly HashSet<string> EmptySet = new();
+
         private readonly IAuthorizationService _authorizationService;
+        private readonly IChunkFileUploadService _chunkFileUploadService;
         private readonly IContentTypeProvider _contentTypeProvider;
         private readonly ILogger _logger;
         private readonly IMediaFileStore _mediaFileStore;
         private readonly IMediaNameNormalizerService _mediaNameNormalizerService;
+        private readonly MediaOptions _mediaOptions;
         private readonly IStringLocalizer S;
         private readonly ITinyPngService _tinyPngService;
 
         public AdminController(
             IAuthorizationService authorizationService,
+            IChunkFileUploadService chunkFileUploadService,
             IContentTypeProvider contentTypeProvider,
             ILogger<AdminController> logger,
             IMediaFileStore mediaFileStore,
@@ -39,12 +44,13 @@ namespace Etch.OrchardCore.TinyPNG.Controllers
             ITinyPngService tinyPngService
             )
         {
-            _allowedFileExtensions = options.Value.AllowedFileExtensions;
             _authorizationService = authorizationService;
+            _chunkFileUploadService = chunkFileUploadService;
             _contentTypeProvider = contentTypeProvider;
             _logger = logger;
             _mediaFileStore = mediaFileStore;
             _mediaNameNormalizerService = mediaNameNormalizerService;
+            _mediaOptions = options.Value;
             S = stringLocalizer;
             _tinyPngService = tinyPngService;
         }
@@ -52,74 +58,85 @@ namespace Etch.OrchardCore.TinyPNG.Controllers
         [Route("/admin/media/upload")]
         [HttpPost]
         [MediaSizeLimit]
-        public async Task<ActionResult<object>> Upload(
-            string path,
-            ICollection<IFormFile> files)
+        public async Task<IActionResult> Upload(string path, string extensions)
         {
             if (!await _authorizationService.AuthorizeAsync(User, Permissions.ManageMedia))
             {
                 return Forbid();
             }
 
-            if (string.IsNullOrEmpty(path))
-            {
-                path = "";
-            }
+            var allowedExtensions = GetRequestedExtensions(extensions, true);
 
-            var result = new List<object>();
+            return await _chunkFileUploadService.ProcessRequestAsync(
+                Request,
 
-            // Loop through each file in the request
-            foreach (var file in files)
-            {
-                // TODO: support clipboard
-
-                if (!_allowedFileExtensions.Contains(Path.GetExtension(file.FileName), StringComparer.OrdinalIgnoreCase))
+                // We need this empty object because the frontend expects a JSON object in the response.
+                (_, _, _) => Task.FromResult<IActionResult>(Ok(new { })),
+                async (files) =>
                 {
-                    result.Add(new
+                    if (string.IsNullOrEmpty(path))
                     {
-                        name = file.FileName,
-                        size = file.Length,
-                        folder = path,
-                        error = S["This file extension is not allowed: {0}", Path.GetExtension(file.FileName)].ToString()
-                    });
+                        path = string.Empty;
+                    }
 
-                    _logger.LogInformation("File extension not allowed: '{File}'", file.FileName);
+                    var result = new List<object>();
 
-                    continue;
-                }
-
-                var fileName = _mediaNameNormalizerService.NormalizeFileName(file.FileName);
-
-                Stream stream = null;
-                try
-                {
-                    var mediaFilePath = _mediaFileStore.Combine(path, fileName);
-                    stream = file.OpenReadStream();
-                    mediaFilePath = await _mediaFileStore.CreateFileFromStreamAsync(mediaFilePath, await _tinyPngService.OptimiseAsync(stream, file.FileName));
-
-                    var mediaFile = await _mediaFileStore.GetFileInfoAsync(mediaFilePath);
-
-                    result.Add(CreateFileResult(mediaFile));
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "An error occurred while uploading a media");
-
-                    result.Add(new
+                    // Loop through each file in the request.
+                    foreach (var file in files)
                     {
-                        name = fileName,
-                        size = file.Length,
-                        folder = path,
-                        error = ex.Message
-                    });
-                }
-                finally
-                {
-                    stream?.Dispose();
-                }
-            }
+                        var extension = Path.GetExtension(file.FileName);
 
-            return new { files = result.ToArray() };
+                        if (!allowedExtensions.Contains(extension))
+                        {
+                            result.Add(new
+                            {
+                                name = file.FileName,
+                                size = file.Length,
+                                folder = path,
+                                error = S["This file extension is not allowed: {0}", extension].ToString()
+                            });
+
+                            if (_logger.IsEnabled(LogLevel.Information))
+                            {
+                                _logger.LogInformation("File extension not allowed: '{File}'", file.FileName);
+                            }
+
+                            continue;
+                        }
+
+                        var fileName = _mediaNameNormalizerService.NormalizeFileName(file.FileName);
+
+                        Stream stream = null;
+                        try
+                        {
+                            var mediaFilePath = _mediaFileStore.Combine(path, fileName);
+                            stream = file.OpenReadStream();
+                            mediaFilePath = await _mediaFileStore.CreateFileFromStreamAsync(mediaFilePath, await _tinyPngService.OptimiseAsync(stream, file.FileName));
+
+                            var mediaFile = await _mediaFileStore.GetFileInfoAsync(mediaFilePath);
+
+                            result.Add(CreateFileResult(mediaFile));
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogError(ex, "An error occurred while uploading a media");
+
+                            result.Add(new
+                            {
+                                name = fileName,
+                                size = file.Length,
+                                folder = path,
+                                error = ex.Message
+                            });
+                        }
+                        finally
+                        {
+                            stream?.Dispose();
+                        }
+                    }
+
+                    return Ok(new { files = result.ToArray() });
+                });
         }
 
         private object CreateFileResult(IFileStoreEntry mediaFile)
@@ -138,6 +155,31 @@ namespace Etch.OrchardCore.TinyPNG.Controllers
                 anchor = new { x = 0.5f, y = 0.5f },
                 attachedFileName = String.Empty
             };
+        }
+
+        private HashSet<string> GetRequestedExtensions(string exts, bool fallback)
+        {
+            if (!string.IsNullOrWhiteSpace(exts))
+            {
+                var extensions = exts.Split(ExtensionSeperator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+                var requestedExtensions = _mediaOptions.AllowedFileExtensions
+                    .Intersect(extensions)
+                    .ToHashSet(StringComparer.OrdinalIgnoreCase);
+
+                if (requestedExtensions.Count > 0)
+                {
+                    return requestedExtensions;
+                }
+            }
+
+            if (fallback)
+            {
+                return _mediaOptions.AllowedFileExtensions
+                    .ToHashSet(StringComparer.OrdinalIgnoreCase);
+            }
+
+            return EmptySet;
         }
     }
 }
